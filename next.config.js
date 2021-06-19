f9fb1c90dbd5ea3cdec40a4541d7368d140fc026
jequex <onyejekwe.harddisk20@gmail.com>
2021-06-19 19:06:28 +0100

    first day

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+module.exports = {
+  env: {
+    MONGO_URI: "mongodb+srv://Jequex:jequexPassword@notes.1tpou.mongodb.net/notesDatabase?retryWrites=true&w=majority"
+  }
+}
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import dbConnect from '../../utils/dbConnect';
 
-export default function handler(req, res) {
+dbConnect()
+
+export default async function hello (req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
